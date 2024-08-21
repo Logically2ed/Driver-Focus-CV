53f0646b39f6011250fb8c61969f154bea3d7065
blake <blake@blakemcfarlane.com>
2024-08-21 12:07:30 -0400

    expreimentation with basic python structuing and cmd argument control.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Driver-Focus-CV
+
+### Questions to answer:
+I want to know how viable it is to check how many people are actively paying attention to something other than the road.
+
+What circumstances are surrounding the driving condition?
+
+What data can be gathered from this research?
+
+Can this be done real time?
+
+If so, how many parameters can be calculated on the fly and what is its accuracy?    
---
+++ main.py
@@ -0,0 +1,20 @@
+import argparse as ap
+
+def start():
+    parser = ap.ArgumentParser(description='Process some integers.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                        help='an integer for the accumulator')
+    parser.add_argument('--sum', dest='accumulate', action='store_const',
+                        const=sum, default=max,
+                        help='sum the integers (default: find the max)')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+
+def main():
+    start()
+    result = 'empty result'
+    return result
+
+if __name__ == "__main__":
+    main()
